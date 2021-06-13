fbb2875f45e7f50615c49e08310f90ca32ffada2
Dryrtan <kleidimars@gmail.com>
2021-06-12 22:39:10 -0300

    Version 2.0

--- upload.py
+++ upload.py
@@ -30,4 +30,3 @@ def sms2(chat_id, sms):
     menu_keyboard = [['Sim'], ['Não']]
     menu_markup = ReplyKeyboardMarkup(menu_keyboard, one_time_keyboard=True, resize_keyboard=True)
     bot.send_message(chat_id=chat_id, text=sms, reply_markup = menu_markup)
-#sms(864397332, 'Teste')
